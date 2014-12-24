9f2dbdb5c4a1e90ae27bf5e1d1b213c6f228d80c
john ko <johnsko89@gmail.com>
2014-12-23 19:14:16 -0500

    Add names to user model and views

--- Gemfile
+++ Gemfile
@@ -38,3 +38,4 @@ gem 'acts_as_votable'
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 gem 'bootstrap-sass', '~> 3.2.0.2'
+gem 'simple_form', '~>3.0.2'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223232539) do
+ActiveRecord::Schema.define(version: 20141224000504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "links", force: true do |t|
     t.string   "title"
@@ -39,6 +50,7 @@ ActiveRecord::Schema.define(version: 20141223232539) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
